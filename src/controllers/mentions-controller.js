fb2439da0c401077cd63a871e6d97876777f0c7b
vagnerasilva <vagnerasilva>
2020-02-04 17:53:12 -0300

    atualizando rota com api na frente

--- src/controllers/mentions-controller.js
+++ src/controllers/mentions-controller.js
@@ -3,12 +3,18 @@ const Mentions = mongoose.model('Mentions');
 
 // list
 exports.listMentions = async (req, res) => {
+  console.log("##### aquii listando #####")
   try {
     const data = await Mentions.find({});
+    console.log("##### aquii listando OK #####")
+    console.log("##### GET #####")
+    console.log("##### aquii listando OK #####")
     res.status(200).send(data);
   } catch (e) {
+    console.log("##### aquii listando ERRO #####")
+   
     res.status(500).send({
-      message: 'Falha ao carregar as menções.'
+         message: 'Falha ao carregar as menções.'
     });
   }
 };
@@ -24,13 +30,17 @@ exports.createMention = async (req, res) => {
     console.log(mention)
 
     await mention.save();
-
+    console.log("##### aquii CRIANDO OK #####")
+    console.log("##### POST #####")
+    console.log("##### aquii CRIANDO OK #####")
     res.status(201).send({
       message: 'Menção cadastrada com sucesso!'
     });
   } catch (e) {
+    console.log("##### aquii ERRO POST OK #####")
     res.status(500).send({
       message: 'Falha ao cadastrar a menção.'
     });
   }
 };
+
--- src/routes/mentions-routes.js
+++ src/routes/mentions-routes.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 const mentionsController = require('../controllers/mentions-controller');
 
-router.get('/', mentionsController.listMentions);
-router.post('/', mentionsController.createMention);
+
+// router.get('/api', (req, res, next) => {
+//     res.status(200).send({
+//       title: 'MentionsAPI',
+//       version: '1.0.0'
+//     });
+//   });
+
+
+// como foi declarado vc deve usar esse caminho app.use('/mentions', mentionsRoutes);
+// localhost:3000/mentions/api   
+router.get('/api', mentionsController.listMentions);
+router.post('/api', mentionsController.createMention);
 
 module.exports = router;
